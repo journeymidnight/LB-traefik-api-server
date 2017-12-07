32d4c0fb8a3420778f851383f4f95ff364e8741e
victoryang <yangtao3601@126.com>
2017-12-07 16:12:51 +0800

    change way to retrieve CN from certificate file

--- src/certs.go
+++ src/certs.go
@@ -8,57 +8,9 @@ import (
         "crypto"
 	"crypto/tls"
         "crypto/x509"
-        "crypto/x509/pkix"
 	"crypto/rsa"
 	"crypto/ecdsa"
 )
-
-var oid = map[string]string{
-    "2.5.4.3":                    "CN",
-    "2.5.4.4":                    "SN",
-    "2.5.4.5":                    "serialNumber",
-    "2.5.4.6":                    "C",
-    "2.5.4.7":                    "L",
-    "2.5.4.8":                    "ST",
-    "2.5.4.9":                    "streetAddress",
-    "2.5.4.10":                   "O",
-    "2.5.4.11":                   "OU",
-    "2.5.4.12":                   "title",
-    "2.5.4.17":                   "postalCode",
-    "2.5.4.42":                   "GN",
-    "2.5.4.43":                   "initials",
-    "2.5.4.44":                   "generationQualifier",
-    "2.5.4.46":                   "dnQualifier",
-    "2.5.4.65":                   "pseudonym",
-    "0.9.2342.19200300.100.1.25": "DC",
-    "1.2.840.113549.1.9.1":       "emailAddress",
-    "0.9.2342.19200300.100.1.1":  "userid",
-}
-
-func getDNFromCert(namespace pkix.Name, sep string) (string, string, error) {
-    subject := []string{}
-    var commonName string
-    for _, s := range namespace.ToRDNSequence() {
-        for _, i := range s {
-            if v, ok := i.Value.(string); ok {
-                if name, ok := oid[i.Type.String()]; ok {
-                    // <oid name>=<value>
-                    subject = append(subject, fmt.Sprintf("%s=%s", name, v))
-                    if name == "CN" {
-                      commonName = v
-                    }
-                } else {
-                    // <oid>=<value> if no <oid name> is found
-                    subject = append(subject, fmt.Sprintf("%s=%s", i.Type.String(), v))
-                }
-            } else {
-                // <oid>=<value in default format> if value is not string
-                subject = append(subject, fmt.Sprintf("%s=%v", i.Type.String, v))
-            }
-        }
-    }
-    return sep + strings.Join(subject, sep), commonName, nil
-}
 
 func X509KeyPair(certPEMBlock, keyPEMBlock []byte) (cert tls.Certificate, cn string, err error) {
 	fmt.Println(certPEMBlock)
@@ -70,7 +22,6 @@ func X509KeyPair(certPEMBlock, keyPEMBlock []byte) (cert tls.Certificate, cn str
       break
     }
     if certDERBlock.Type == "CERTIFICATE" {
-	    fmt.Println("found one...")
       cert.Certificate = append(cert.Certificate, certDERBlock.Bytes)
     }
   }
@@ -114,9 +65,10 @@ func X509KeyPair(certPEMBlock, keyPEMBlock []byte) (cert tls.Certificate, cn str
     return
   }
 
-  _, cn, err = getDNFromCert(x509Cert.Subject, "/")
-  if err != nil {
-    fmt.Println("err")
+  if x509Cert != nil {
+    if x509Cert.Subject != nil {
+      cn = x509Cert.Subject.CommonName
+    }
   }
   return
 }
